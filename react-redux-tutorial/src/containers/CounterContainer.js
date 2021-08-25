10d6e8c4393b7d73224e5ec819ed8f46dc5acbc8
PiaoZhengxun <pjh951218@naver.com>
2021-08-25 12:38:27 +0900

    chapter 17
    understanding react redux

---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,35 @@
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
+
+export default CounterContainer;
